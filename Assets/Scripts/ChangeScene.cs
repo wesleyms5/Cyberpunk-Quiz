d1883b7565629ea450a67ffbda5043dd2faba75f
wesley <100660344+wesleyms5@users.noreply.github.com>
2024-06-20 18:42:17 -0300

    Project Files

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+
+        SceneManager.LoadScene("QuizScene");
+
+
+    }
+
+    public void LoadScene2()
+    {
+
+        SceneManager.LoadScene("QuizScene2");
+
+
+    }
+
+    public void LoadScene3()
+    {
+
+        SceneManager.LoadScene("QuizScene3");
+
+
+    }
+
+    public void LoadScene4()
+    {
+
+        SceneManager.LoadScene("QuizScene4");
+
+
+    }
+}
---
+++ Assets/Scripts/ScoreManagement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+
+    public Text gameText;
+    int scoreQuiz = 0;
+
+
+    public void ScoreIncreaser()
+    {
+
+        scoreQuiz++;
+
+    }
+
+}
